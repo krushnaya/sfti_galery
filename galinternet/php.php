726532ac902104f9f5f737c07e30edcab528cf1a
Михаил <138455173+krushnaya@users.noreply.github.com>
2025-02-03 13:22:25 +0500

    Delete galinternet directory

--- galinternet/php.php
+++
@@ -1,8 +0,0 @@
-<!-- ?php/*
-$handle = opendir(dirname(realpath(__FILE__)).'/images1/');
-while($file = readdir($handle)){
-  if($file !== '.' && $file !== '..'){
-    echo '<img style="width:100% " class="img-fluid" src="/images1/'.$file.'" border="0" />';
-  }
-}*/
-?>  -->
